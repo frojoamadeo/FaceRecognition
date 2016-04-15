4cdb3e093018c8e2a534a50557f1b6fb8fe865b5
FelipeRonR <rojoamadeoa@gmail.com>
2016-04-15 17:47:44 -0300

    Error Folder

---
+++ FaceRecognition/Controllers/ErrorsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FaceRecognition.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        //
+        // GET: /Errors/
+        public ActionResult Index()
+        {
+            return View();
+        }
+      
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+	}
+}
